adbc7bb57dc62674d1445a7951bcc7496cb84289
Nashira Emery <nashiraemery1@gmail.com>
2017-12-27 17:47:23 -0500

    resolved seed file, controller, and model

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,59 +1,45 @@
 class JobsController < ApplicationController
-  # include Nokogiri
-  before_action :find_job, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @jobs = Job.scraper
-
-    # if params[:category].blank?
-    #   @jobs = Job.all.order("created_at DESC")
-    # else
-    #   @category_id = Category.find_by(name: params[:category]).id
-    #   @jobs = Job.where(category_id: @category_id).order("created_at DESC")
-    # end
-  end
-
-  def show
-  end
-
-  def new
-    @job = Job.new
-  end
-
-  def create
-    @job = Job.new(jobs_params)
-
-    if @job.save
-      redirect_to @job
-    else
-      render "new"
+    before_action :find_job, only: [:show, :edit, :update, :destroy]
+    def index
+        @craig_jobs = Job.scraper(params[:category].presence)
+        if params[:category].blank?
+            @jobs = Job.all.order("created_at DESC")
+        else
+            @category_id = Category.find_by(name: params[:category]).id
+            @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+        end
     end
-  end
-
-  def edit
-  end
-
-  def update
-    if @job.update(jobs_params)
-      redirect_to @job
-    else
-      render "Edit"
+    def show
     end
-  end
-
-  def destroy
-    @job.destroy
-    redirect_to root_path
-  end
-
-  private
-
-  def jobs_params
-    params.require(:job).permit(:title, :description, :company, :url, :category_id)
-  end
-
-  def find_job
-    @job = Job.find(params[:id])
-  end
-
+    def new
+        @job = Job.new
+    end
+    def create
+        @job = Job.new(jobs_params)
+        if @job.save
+            redirect_to @job
+        else
+            render "new"
+        end
+    end
+    def edit
+    end
+    def update
+        if @job.update(jobs_params)
+            redirect_to @job
+        else
+            render "edit_job"
+        end
+    end
+    def destroy
+        @job.destroy
+        redirect_to root_path
+    end
+    private
+    def jobs_params
+        params.require(:job).permit(:title, :description, :company, :url, :category_id)
+    end
+    def find_job
+        @job = Job.find(params[:id])
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CATEGORIES = ["Full-Time", "Part-Time", "Contract", "Employee's Choice"]
+
+CATEGORIES.each do |cat|
+	Category.create!({name: cat})
+end
